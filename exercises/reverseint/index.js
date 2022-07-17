fcd59d0c2b3308299d9285886706a8d8bb82abf4
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-16 22:02:15 -0600

    solved algo

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const stringifiedNumber = n.toString();
+  let isNegative = false;
+  let reversedInteger = "";
+
+  for (let num of stringifiedNumber) {
+    if (num === "-") {
+      isNegative = true;
+      continue;
+    }
+    reversedInteger = num + reversedInteger;
+  }
+
+  return isNegative
+    ? Math.abs(reversedInteger) * -1
+    : Math.abs(reversedInteger);
+}
+
+console.log(reverseInt(-93));
 
 module.exports = reverseInt;
