49c9c76ac9f83dfc7668e6313311951ff725b856
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-26 19:11:16 -0600

    added matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -79,11 +79,11 @@ function matrix(n) {
 
   let counter = 1;
 
-  const startColumn = 0;
-  const endColumn = n - 1;
+  let startColumn = 0;
+  let endColumn = n - 1;
 
-  const startRow = 0;
-  const endRow = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
 
   while (startColumn <= endColumn && startRow <= endRow) {
     for (let i = startColumn; i <= endColumn; i++) {
@@ -92,26 +92,56 @@ function matrix(n) {
     }
     startRow++;
 
-    for (let i = startRow; i <= endRow; i++) {
+    for (let i = startRow; i <= endColumn; i++) {
       result[i][endColumn] = counter;
       counter++;
     }
-    endColumn--;
- 
-    for (let i = endColumn; i >= startColumn; i--) {
-      result[endRow][i] = counter;
-      counter++;
-    } 
-    endRow--;
+    endColumn--
 
-    for (let i = endRow; i >= startRow; i--) {
-      result[i][startColumn] = counter;
-      counter++;
-    }
-    startColumn++;
+    for
   }
 
-  return result;
+  // const result = [];
+
+  // for (let i = 0; i < n; i++) {
+  //   result.push([]);
+  // }
+
+  // let counter = 1;
+
+  // const startColumn = 0;
+  // const endColumn = n - 1;
+
+  // const startRow = 0;
+  // const endRow = n - 1;
+
+  // while (startColumn <= endColumn && startRow <= endRow) {
+  //   for (let i = startColumn; i <= endColumn; i++) {
+  //     result[startRow][i] = counter;
+  //     counter++;
+  //   }
+  //   startRow++;
+
+  //   for (let i = startRow; i <= endRow; i++) {
+  //     result[i][endColumn] = counter;
+  //     counter++;
+  //   }
+  //   endColumn--;
+
+  //   for (let i = endColumn; i >= startColumn; i--) {
+  //     result[endRow][i] = counter;
+  //     counter++;
+  //   }
+  //   endRow--;
+
+  //   for (let i = endRow; i >= startRow; i--) {
+  //     result[i][startColumn] = counter;
+  //     counter++;
+  //   }
+  //   startColumn++;
+  // }
+
+  // return result;
 }
 
 module.exports = matrix;
