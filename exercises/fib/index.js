f2358d3d40cecc743c775804015a4375c0b674e3
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-26 19:05:06 -0600

    added memoization

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,23 +8,62 @@
 // Example:
 //   fib(4) === 3
 
-// O(n) or linear complexity
 // it is a for loop that has a fix number going to a fix target so linear
 function fib(n) {
   const results = [0, 1];
 
   for (let i = 2; i <= n; i++) {
-    //previews two records
-    const previewsNumber = results[i - 1]; //1
-    const previewsTwoNumbers = results[i - 2]; //0
+    const previousNumber = results[i - 1];
+    const previewsTwoNumbers = results[i - 2];
 
-    results.push(previewsNumber + previewsTwoNumbers);
+    results.push(previousNumber + previewsTwoNumbers);
   }
 
   return results[n];
 }
+console.log(fib(4));
 
+////////////////////////////////////////////////////////////////////////////////////////////////
 // recursive Solution
-function fibonacci() {}
+// log(n)
+// bad time complexity
+// Exponential runtime because it calls the function time and time again (look at the diagram)
+// for every call to n we are calling another function
+function fibonacci(n) {
+  if (n < 2) return n;
+
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+console.log(fibonacci(4));
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+function memoize(fn) {
+  const cache = {};
+  
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return memoizedFib(n - 1) + memoizedFib(n - 2);
+}
+
+const memoizedFib = memoize(slowFib);
+console.log(fib(4));
+
+// to improve the runtime complexity you can memoize
 
 module.exports = fib;
