827cd10a502017a6ab789da271ce3460a2add5f8
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-27 19:21:01 -0600

    started linkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,21 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = this.head || data;
+  }
+}
 
 module.exports = { Node, LinkedList };
