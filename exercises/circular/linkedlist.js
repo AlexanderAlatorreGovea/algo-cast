63206d803e1cbcd8359e37468d1c59cfd5180ef0
Alexander <Alexander_alatorre1993@hotmail.com>
2022-07-30 22:26:50 -0600

    commented out iterator

--- exercises/circular/linkedlist.js
+++ exercises/circular/linkedlist.js
@@ -166,13 +166,13 @@ class LinkedList {
     }
   }
 
-  *[Symbol.iterator]() {
-    let node = this.head;
-    while (node) {
-      yield node;
-      node = node.next;
-    }
-  }
+  // *[Symbol.iterator]() {
+  //   let node = this.head;
+  //   while (node) {
+  //     yield node;
+  //     node = node.next;
+  //   }
+  // }
 }
 
 module.exports = { Node, LinkedList };
