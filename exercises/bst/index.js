e020dcc61bc5026514171381482e6fa644999448
Alexander <Alexander_alatorre1993@hotmail.com>
2022-08-02 19:54:47 -0600

    added constructor to BTS

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,12 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
 
 module.exports = Node;
