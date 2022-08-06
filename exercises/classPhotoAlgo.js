b64ae68183471e79ca39b43794f8fa802899d981
Alexander <Alexander_alatorre1993@hotmail.com>
2022-08-06 14:51:29 -0600

    solved algo

--- exercises/classPhotoAlgo.js
+++ exercises/classPhotoAlgo.js
@@ -2,20 +2,16 @@ function classPhotos(redShirtHeights, blueShirtHeights) {
   // Write your code here.
   redShirtHeights.sort((a, b) => a - b);
   blueShirtHeights.sort((a, b) => a - b);
-  let counter = 0;
+
   const shirtColor = redShirtHeights[0] < blueShirtHeights[0] ? "RED" : "BLUE";
 
-  for (let i = 0; i < biggestHeight.length; i++) {
+  for (let i = 0; i < redShirtHeights.length; i++) {
     const redShirtHeight = redShirtHeights[i];
-    const blueshirtHeight = blueShirtHeights[i];
+    const blueShirtHeight = blueShirtHeights[i];
 
     if (shirtColor === "RED") {
-      if (redShirtHeight >= blueshirtHeight) {
-        return false;
-      }
-    } else if (blueshirtHeight >= redShirtHeight) {
-      return false;
-    }
+      if (redShirtHeight >= blueShirtHeight) return false;
+    } else if (blueShirtHeight >= redShirtHeight) return false;
   }
 
   return true;
